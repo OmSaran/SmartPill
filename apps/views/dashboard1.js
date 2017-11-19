5add5cd6528641153db757c5054bc1c1b350a90b
swarnashree <ms.swarnashree@gmail.com>
2017-11-19 18:36:14 +0530

    v1.0.0_dashboard

---
+++ apps/views/dashboard1.js
@@ -0,0 +1,191 @@
+var app = angular.module('dashboardApp',[]);
+
+function check_authBearer()
+{
+    console.log("checkauthbearer");
+    if(localStorage.getItem("authBearer")=="")
+        return false;
+    return true;
+}
+
+app.controller('navbar-controller',function($scope){
+
+
+    console.log("navbar-controller");
+    $scope.username = localStorage.getItem("username");
+
+    $scope.logout = function()
+    {
+        localStorage.setItem("authBearer","");
+        localStorage.setItem("username","");
+        window.location = "/index.html";
+    }
+});
+
+app.controller('sidebar-controller',function($scope, $http){
+
+    console.log("sidebar-controller");
+     request = 'Bearer '+localStorage.getItem("authBearer");
+    console.log("token "+localStorage.getItem("authBearer"));
+
+    
+    $scope.patients = [];
+    $scope.patientDetails = [];
+    $scope.selectedPatientName = "";
+    $scope.dosageParam = [];
+
+ 
+    if(check_authBearer()) 
+    {
+        $http({
+            method: 'GET',
+            url: '/api/doc/patient',
+            headers: {
+                'Authorization': request
+                
+            }}).then(function(result) {
+                
+                $scope.patients = result.data;
+                
+            
+            }, 
+            function(error) {
+               console.log("error "+error.status);
+                
+            });
+    }
+
+    
+
+    $scope.getPatientDetails = function(patient){
+
+                                    $scope.selectedPatientName = patient.name;
+                                   
+                                    var patientUsername = patient.username;
+                                    var request = 'Bearer '+localStorage.getItem("authBearer");
+                                    if(check_authBearer()) 
+                                    {
+                                        $http({
+                                            method: 'GET',
+                                            url: '/api/doc/patient/'+patientUsername,
+                                            headers: {
+                                                'Authorization': request
+                                                
+                                            }}).then(function(result) {
+                                                
+                                               
+                                                console.log(result);
+                                                $scope.patientDetails = result.data;
+                                                
+                                            
+                                            }, 
+                                            function(error) {
+                                                alert(error.status);
+                                               console.log("error "+error.status);
+                                                
+                                            });
+                                    }
+
+                                };
+
+
+
+        $scope.addDosageTime = function(){
+
+                                    ($scope.dosageParam).push($scope.dosage);
+                                    console.log($scope.dosageParam);
+                                    $scope.dosage = "";
+                                    document.getElementById("dosageField").style.backgroundColor = "yellow";
+                                    
+
+
+                                };
+
+        $scope.updateCourse = function(){
+
+
+                               idParam = ($scope.patientDetails[0]).id;
+                               pillNameParam = $scope.pillName; 
+                               pillDescriptionParam = $scope.pillDescription;
+                               courseParam = $scope.course;
+                               var request = 'Bearer '+localStorage.getItem("authBearer");
+
+                               var requestJSON = {pill:pillNameParam, course:courseParam, description:pillDescriptionParam, dosage: $scope.dosageParam};
+                               var req = JSON.stringify(requestJSON); 
+                               alert(idParam+" "+pillNameParam+" "+pillDescriptionParam+" "+courseParam+" "+$scope.dosageParam);
+
+                               if(check_authBearer()) 
+                               {
+                                   $http({
+                                       method: 'POST',
+                                       url: '/api/pillbottle/course/'+idParam,
+                                       data: req,
+                                       headers: {
+                                           'Authorization': request,
+                                           'Content-Type': 'application/json'
+                                           
+                                       }}).then(function(result) {
+                                           
+                                          
+                                           alert("success "+result.status);
+                                           $scope.pillName = "";
+                                           $scope.pillDescription = "";
+                                           $scope.course = 0;
+                                           $scope.dosageParam = [];
+                                        
+                                       
+                                       }, 
+                                       function(error) {
+                                           alert(error.status);
+                                          console.log("error "+error.status);
+                                           
+                                       });
+                               }
+
+
+
+
+
+                        };
+
+
+        $scope.deleteCourse = function(){
+                                
+                                var response = window.confirm("Are you sure you want to delete the course for "+$scope.selectedPatientName+"?");
+                                var request = 'Bearer '+localStorage.getItem("authBearer");
+                                if(response)
+                                {
+                                    if(check_authBearer()) 
+                                    {
+                                        $http({
+                                            method: 'DELETE',
+                                            url: '/api/pillbottle/course/'+($scope.patientDetails[0]).id,
+                                            headers: {
+                                                'Authorization': request
+                                                                                
+                                            }}).then(function(result) {
+                                                                                                  
+                                                window.location = '/dashboard.html';        
+                                                                            
+                                            }, 
+                                            function(error) {
+                                               
+                                                alert("Oops! There was an ERROR with status "+error.status+". Try Again ");
+                                                console.log("error "+error.status);
+                                                
+                                                                                
+                                            });
+                                    }
+                                }
+                                
+                                
+                                
+                    };
+
+
+});
+
+
+
+
+
